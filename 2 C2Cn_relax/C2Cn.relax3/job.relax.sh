6e557de402ae13fc106fd0ff08f732f2857c6c45
kennethsharman <kenneth.sharman@ucalgary.ca>
2022-11-15 11:05:51 -0700

    ebands

---
+++ 2 C2Cn_relax/C2Cn.relax3/job.relax.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 100.12 G
+
+#SBATCH --time=1-00:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=45G
+
+#SBATCH --mail-user=kenneth.sharman@ucalgary.ca
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020
+module load intel/2020.1.217
+module load openmpi/4.0.3
+module load quantumespresso/6.8
+
+srun pw.x <hBN.in> hBN.out
---
+++ 3 C2Cn_ebands/README.txt
@@ -0,0 +1,8 @@
+
+Execute the jobs in the following order
+	1)	job.scf
+	2)	job.nscf
+	3)	job.bands
+	4)	job.bands_pp
+
+Plot hBN.bands.dat.gnu file and compare to the C2CN_ebands.png I plotted.
